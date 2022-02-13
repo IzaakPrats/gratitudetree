802a923e145066b65e84ff0e2b936589fc78388a
Padmini Pyapali <padminipyapali@Padminis-MacBook-Pro-2.local>
2022-02-13 16:49:30 -0700

    Add back component

---
+++ packages/next-dapp/components/GratitudeNftMintContainer.tsx
@@ -0,0 +1,48 @@
+import { BigNumber } from "ethers";
+import { useEffect } from "react";
+import useContractWithSigner from "../hooks/useContractWithSigner";
+
+const CONTRACT_NAME = "GratitudeNFT";
+
+const GratitudeNftMintContainer = () => {
+  const contract = useContractWithSigner({ contractName: CONTRACT_NAME });
+
+  const onNewGratitudeMinted = (
+    owner: string,
+    tokenId: BigNumber,
+    gratitudeData: {}
+  ) => {
+    console.log("New nft %d minted by %s.", tokenId.toNumber(), owner);
+    console.log("NFT data: ", gratitudeData);
+  };
+
+  useEffect(() => {
+    if (contract.signer) {
+      contract.on("NewGratitudeMinted", onNewGratitudeMinted);
+    }
+
+    return () => {
+      if (contract) {
+        contract.off("NewGratitudeMinted", onNewGratitudeMinted);
+      }
+    };
+  }, [contract, contract.signer]);
+
+  const mintNft = async () => {
+    try {
+      const mintNftTxn = await contract.mint("title", "message", "location");
+      console.log("Minted.", mintNftTxn.hash);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="flex-column rounded border shadow">
+      <p>Did you meet someone this week who taught you something new?</p>
+      <button onClick={mintNft}>Mint</button>
+    </div>
+  );
+};
+
+export default GratitudeNftMintContainer;
