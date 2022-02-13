77576200a44ea082405570b0ea7eb83cde1c9354
Padmini Pyapali <padminipyapali@Padminis-MacBook-Pro-2.local>
2022-02-13 16:55:04 -0700

    Add back component.

---
+++ packages/next-dapp/components/GratitudeNftContractInfo.tsx
@@ -0,0 +1,13 @@
+import ContractInfo from "./ContractInfo";
+
+const CONTRACT_NAME = "GratitudeNFT";
+
+const GratitudeNftContractInfo = () => {
+  return (
+    <div className="font-mono">
+      <ContractInfo contractName={CONTRACT_NAME} />
+    </div>
+  );
+};
+
+export default GratitudeNftContractInfo;
