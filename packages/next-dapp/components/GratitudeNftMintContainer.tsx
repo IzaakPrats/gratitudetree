a74a0e76ad12b3a49ccdc21def6d1d150d7f4cc1
Padmini Pyapali <padminipyapali@Padminis-MacBook-Pro-2.local>
2022-02-13 18:29:40 -0700

    Disable button and clear form while TX is happening.

--- packages/next-dapp/components/GratitudeNftMintContainer.tsx
+++ packages/next-dapp/components/GratitudeNftMintContainer.tsx
@@ -6,9 +6,16 @@ const CONTRACT_NAME = "GratitudeNFT";
 
 const GratitudeNftMintContainer = () => {
   const contract = useContractWithSigner({ contractName: CONTRACT_NAME });
-  const [title, setTitle] = useState<String>("");
-  const [message, setMessage] = useState<String>("");
-  const [location, setLocation] = useState<String>("");
+  const [isMinting, setIsMinting] = useState<boolean>(false);
+  const [title, setTitle] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
+  const [location, setLocation] = useState<string>("");
+
+  const resetForm = () => {
+    setTitle("");
+    setMessage("");
+    setLocation("");
+  };
 
   const onNewGratitudeMinted = (
     owner: string,
@@ -33,46 +40,56 @@ const GratitudeNftMintContainer = () => {
 
   const mintNft = async () => {
     try {
+      setIsMinting(true);
+      resetForm();
       const mintNftTxn = await contract.mint(title, message, location);
+      setIsMinting(false);
       console.log("Minted.", mintNftTxn.hash);
     } catch (error) {
+      setIsMinting(false);
       console.log(error);
     }
   };
 
   return (
-    <div className="m-auto flex flex-col my-16 space-y-4 p-8 w-full max-w-lg border shadow rounded-lg text-slate-600">
-      <p className="text-lg font-bold">What are you grateful for?</p>
-      <div>
-        <p className="text-s font-bold">Title</p>
-        <textarea
-          className="p-2 w-full border shadow rounded-lg"
-          rows={1}
-          onChange={(e) => setTitle(e.target.value)}
-        />
+    <div>
+      <div className="m-auto flex flex-col my-16 space-y-4 p-8 w-full max-w-lg border shadow rounded-lg text-slate-600">
+        <p className="text-lg font-bold">What are you grateful for?</p>
+        <div>
+          <p className="text-s font-bold">Title</p>
+          <textarea
+            className="p-2 w-full border shadow rounded-lg"
+            value={title}
+            rows={1}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div>
+          <p className="text-s font-bold">Message</p>
+          <textarea
+            className="p-2 w-full border shadow rounded-lg"
+            rows={4}
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+        </div>
+        <div>
+          <p className="text-s font-bold">Location</p>
+          <textarea
+            className="p-2 w-full border shadow rounded-lg"
+            rows={1}
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </div>
+        <button
+          className="py-2 px-4 rounded rounded-lg shadow bg-indigo-400 hover:bg-indigo-200 disabled:bg-slate-200 text-slate-700 font-bold"
+          disabled={isMinting}
+          onClick={mintNft}
+        >
+          Mint
+        </button>
       </div>
-      <div>
-        <p className="text-s font-bold">Message</p>
-        <textarea
-          className="p-2 w-full border shadow rounded-lg"
-          rows={4}
-          onChange={(e) => setMessage(e.target.value)}
-        />
-      </div>
-      <div>
-        <p className="text-s font-bold">Location</p>
-        <textarea
-          className="p-2 w-full border shadow rounded-lg"
-          rows={1}
-          onChange={(e) => setLocation(e.target.value)}
-        />
-      </div>
-      <button
-        className="py-2 px-4 rounded rounded-lg shadow bg-indigo-400 hover:bg-indigo-200 text-slate-700 font-bold"
-        onClick={mintNft}
-      >
-        Mint
-      </button>
     </div>
   );
 };
