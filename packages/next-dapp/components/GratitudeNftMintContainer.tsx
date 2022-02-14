a340579fdf67eed22daf12328b561343c246b73a
Padmini Pyapali <padminipyapali@Padminis-MacBook-Pro-2.local>
2022-02-13 18:07:30 -0700

    More styling

--- packages/next-dapp/components/GratitudeNftMintContainer.tsx
+++ packages/next-dapp/components/GratitudeNftMintContainer.tsx
@@ -1,11 +1,14 @@
 import { BigNumber } from "ethers";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useContractWithSigner from "../hooks/useContractWithSigner";
 
 const CONTRACT_NAME = "GratitudeNFT";
 
 const GratitudeNftMintContainer = () => {
   const contract = useContractWithSigner({ contractName: CONTRACT_NAME });
+  const [title, setTitle] = useState<String>("");
+  const [message, setMessage] = useState<String>("");
+  const [location, setLocation] = useState<String>("");
 
   const onNewGratitudeMinted = (
     owner: string,
@@ -30,7 +33,7 @@ const GratitudeNftMintContainer = () => {
 
   const mintNft = async () => {
     try {
-      const mintNftTxn = await contract.mint("title", "message", "location");
+      const mintNftTxn = await contract.mint(title, message, location);
       console.log("Minted.", mintNftTxn.hash);
     } catch (error) {
       console.log(error);
@@ -38,9 +41,32 @@ const GratitudeNftMintContainer = () => {
   };
 
   return (
-    <div className="m-auto flex flex-col my-16 space-y-4 p-8 w-full max-w-lg border shadow rounded-lg">
-      <p className="text-lg text-slate-600">What are you grateful for?</p>
-      <textarea className="p-2 w-full border shadow rounded-lg" rows={4} />
+    <div className="m-auto flex flex-col my-16 space-y-4 p-8 w-full max-w-lg border shadow rounded-lg text-slate-600">
+      <p className="text-lg font-bold">What are you grateful for?</p>
+      <div>
+        <p className="text-s font-bold">Title</p>
+        <textarea
+          className="p-2 w-full border shadow rounded-lg"
+          rows={1}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+      <div>
+        <p className="text-s font-bold">Message</p>
+        <textarea
+          className="p-2 w-full border shadow rounded-lg"
+          rows={4}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+      </div>
+      <div>
+        <p className="text-s font-bold">Location</p>
+        <textarea
+          className="p-2 w-full border shadow rounded-lg"
+          rows={1}
+          onChange={(e) => setLocation(e.target.value)}
+        />
+      </div>
       <button
         className="py-2 px-4 rounded rounded-lg shadow bg-indigo-400 hover:bg-indigo-200 text-slate-700 font-bold"
         onClick={mintNft}
