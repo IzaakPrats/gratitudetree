581a1617e95020be4bd524be16d0126a9732f012
Izaak Prats <izaak.prats@gmail.com>
2022-02-19 13:09:27 -0700

    Pull in txn history and process.

--- packages/next-dapp/pages/api/metadata/[network]/[tokenId].ts
+++ packages/next-dapp/pages/api/metadata/[network]/[tokenId].ts
@@ -8,12 +8,20 @@ import type { GratitudeData } from '../../../../data/types/GratitudeNFT'
 */
 
 const ALCHEMY_KEY = 'ligrFYullfEd6hPhts46CKSCt4QalP9A'
+const NETWORK = 'goerli'
+const LIKES_WALLET_ADDRESS = '0xf3e02c1ff387d4dB5EaAc451c2fF80bc94143C43'
+const ETHERSCAN_KEY = 'W7RW8DPMATZAXQI2VMMA8258W9YSPSBDHR'
+
+const convert = (from: any, to: any) => (str: any) =>
+  Buffer.from(str, from).toString(to)
+const utf8ToHex = convert('utf8', 'hex')
+const hexToUtf8 = convert('hex', 'utf8')
 
 export default async function handler(req: any, res: any) {
   const { network, tokenId } = req.query
 
   // Setup web3
-  const provider = new ethers.providers.AlchemyProvider('goerli', ALCHEMY_KEY)
+  const provider = new ethers.providers.AlchemyProvider(NETWORK, ALCHEMY_KEY)
   const contractAddress = JSON.parse(
     fs.readFileSync(`./data/GratitudeNFT/${network}.json`, 'utf8')
   )['address']
@@ -26,6 +34,35 @@ export default async function handler(req: any, res: any) {
   // Grab gratitude data from the chain
   const chainData: GratitudeData = await gratitudeNft.metadata(tokenId)
 
+  // Setup Etherscan Provider
+  const etherscanProvider = new ethers.providers.EtherscanProvider(
+    NETWORK,
+    ETHERSCAN_KEY
+  )
+
+  // Get history
+  const likesWalletHistory: any[] = await etherscanProvider.getHistory(
+    LIKES_WALLET_ADDRESS
+  )
+  const numberOfLikes = likesWalletHistory
+    .map((transaction) => {
+      const { data } = transaction
+      try {
+        return JSON.parse(hexToUtf8(data.substring(2).replace('\n', '')))
+      } catch (error: any) {
+        return null
+      }
+    })
+    .filter((obj) => obj != null)
+    .reduce((sum, likeData) => {
+      const { tokenId: likedTokenId } = likeData
+      if (likedTokenId == tokenId) {
+        return sum + 1
+      }
+    }, 0)
+
+  // Process history for likes
+
   // Compose metadata
   const metadata = {
     name: chainData.title,
@@ -33,11 +70,7 @@ export default async function handler(req: any, res: any) {
     attributes: [
       {
         trait_type: 'Likes',
-        value: 0,
-      },
-      {
-        trait_type: 'References',
-        value: 0,
+        value: numberOfLikes,
       },
     ],
     image_data: '',
